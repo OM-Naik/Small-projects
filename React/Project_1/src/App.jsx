af2f549867fd5d236a831ea74056f9392be8e9de
OM-Naik <osnaik23@gmail.com>
2026-02-26 01:01:11 +0530

    Refactor App and Card components to manage user data and display user profiles

--- React/Project_1/src/App.jsx
+++ React/Project_1/src/App.jsx
@@ -1,12 +1,87 @@
-/* deleted previous all contex. Becoz its not needed for this file. */
-import React from 'react'
-import Card from './components/Card'
+import React, { useState } from 'react'
+import Card from './components/card'
+
+const App = () => {
+
+  const [userName, setUserName] = useState('')
+  const [userRole, setUserRole] = useState('')
+  const [imageURL, setImageURL] = useState('')
+  const [userDesc, setUserDesc] = useState('')
+
+  const [allUsers, setAllUsers] = useState([])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    setAllUsers([...allUsers, { userName, userRole, userDesc, imageURL }])
+
+    setUserName('')
+    setUserRole('')
+    setUserDesc('')
+    setImageURL('')
+  }
+
+  const deleteHandler = (idx) => {
+    const copyUsers = [...allUsers]
+
+    copyUsers.splice(idx, 1)
+
+    setAllUsers(copyUsers)
+  }
 
 
-function App() {
   return (
-    <div>
-      <Card />
+    <div className='h-screen bg-black text-white'>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='px-2 py-2 flex flex-wrap'>
+
+        <input
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
+          type="text"
+          placeholder='Enter your name' />
+
+        <input
+          value={imageURL}
+          onChange={(e) => {
+            setImageURL(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
+          type="text"
+          placeholder='Image URL' />
+
+        <input
+          value={userRole}
+          onChange={(e) => {
+            setUserRole(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
+          type="text"
+          placeholder='Enter Role' />
+
+        <input
+          value={userDesc}
+          onChange={(e) => {
+            setUserDesc(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
+          type="text"
+          placeholder='Enter Description' />
+
+
+        <button className=' px-5 py-2 text-xl active:scale-95 cursor-pointer font-semibold bg-emerald-700 rounded m-2 w-[97%]'>Create User</button>
+      </form>
+      <div className='px-4 py-10 gap-4 flex flex-wrap'>
+
+        {allUsers.map(function (elem, idx) {
+          return <Card idx={idx} elem={elem} deleteHandler={deleteHandler} />
+        })}
+
+      </div>
     </div>
   )
 }
