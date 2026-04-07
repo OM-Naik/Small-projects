2f2f8f623f9ebae7be513b4aaff275eed7263d74
OM-Naik <osnaik23@gmail.com>
2026-04-08 00:27:12 +0530

    enhance comments for clarity on request handling in Server.js

--- Backend/API-Day_1/Server.js
+++ Backend/API-Day_1/Server.js
@@ -2,7 +2,8 @@ const express = require("express")
 
 const app = express() // server instance create krna
 
-//get request
+//get request --> to get data from server
+//for "Get" we use Render.com and create a server
 app.get('/', (req, res) => {
     res.send("Hello world")
 })
@@ -15,13 +16,20 @@ app.get("/home", (req, res) => {
     res.send("This is Home page")
 })
 
-//post request
+//post request --> to send data to server
+//for "Post" we use Postman and create a server
 const notes = []
 app.use(express.json()) // json data ko parse(convert) krne ke liye middleware use krna
 
 app.post("/notes", (req, res) => {
     console.log(req.body) // request body ko console me print krna
     res.send("notes created")
+})
+
+//delete request --> to delete data from server
+app.delete("/notes/:index", (req, res) => {
+    delete notes[req.params.index] // delete the note at the specified index
+    res.send("note deleted successfully")
 })
 
 app.listen(3000) // server start krna
