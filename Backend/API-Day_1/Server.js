c53f74b0ee56e73b2f2ca012481f21228fdc101f
OM-Naik <osnaik23@gmail.com>
2026-04-07 22:53:48 +0530

    updated

--- Backend/API-Day_1/Server.js
+++ Backend/API-Day_1/Server.js
@@ -4,17 +4,29 @@ const app = express();
 
 app.use(express.json());
 
-const notes = []
+// const notes = []
 
-app.post('/notes', (req, res) => {
-    console.log(req.body);
+// app.post('/notes', (req, res) => {
+//     console.log(req.body);
 
-    notes.push(req.body);
+//     notes.push(req.body);
 
-    res.send('Note added successfully');
+//     res.send('Note added successfully');
 
+// })
+
+// app.get('/notes', (req, res) => {
+//     res.send(notes);
+// })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API');
 })
 
-app.get('/notes', (req, res) => {
-    res.send(notes);
+app.get('/about', (req, res) => {
+    res.send('This is a simple API built with Express.js');
+})
+
+app.get('/home', (req, res) => {
+    res.send('This is the home page');
 })
