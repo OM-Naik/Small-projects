fbcf2b46e7af5eba5650694c61281aa7e19bb585
OM-Naik <osnaik23@gmail.com>
2026-04-07 23:58:15 +0530

    add comments for clarity on request handling

--- Backend/API-Day_1/Server.js
+++ Backend/API-Day_1/Server.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const app = express() // server instance create krna
 
+//get request
 app.get('/', (req, res) => {
     res.send("Hello world")
 })
@@ -12,6 +13,13 @@ app.get("/about", function (req, res) {
 
 app.get("/home", (req, res) => {
     res.send("This is Home page")
+})
+
+//post request
+const notes = []
+
+app.post("/notes", (req, res) => {
+    res.send("notes created")
 })
 
 app.listen(3000) // server start krna
