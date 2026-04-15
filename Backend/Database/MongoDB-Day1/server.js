59cc38e18ab8b46c41d4beb93f4d0fefdb1008fe
OM-Naik <osnaik23@gmail.com>
2026-04-16 00:45:51 +0530

    refactor: reorganize server structure by creating app.js for server configuration

---
+++ Backend/Database/MongoDB-Day1/server.js
@@ -0,0 +1,5 @@
+/*
+It is to start server and connect it to database.
+*/
+
+const app = require("./src/app") // app.js file ko import krna
---
+++ Backend/Database/MongoDB-Day1/src/app.js
@@ -0,0 +1,13 @@
+/*
+
+It is use for server creation and configuration. It is used to create a server and handle the requests and responses.
+
+
+*/
+
+const express = require("express")
+
+const app = express() // server instance create krna
+
+
+module.exports = app
