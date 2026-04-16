fea3de5b3a497ac44facfdd74b41536fcbd2a994
OM-Naik <osnaik23@gmail.com>
2026-04-16 22:57:40 +0530

    updated

--- Backend/Database/MongoDB-Day1/server.js
+++ Backend/Database/MongoDB-Day1/server.js
@@ -3,3 +3,24 @@ It is to start server and connect it to database.
 */
 
 const app = require("./src/app") // app.js file ko import krna
+const mongoose = require("mongoose") // mongoose library ko import krna
+
+const dns = require('node:dns/promises');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+
+// connect to MongoDB database
+function connectToDatabase() {
+    mongoose.connect("mongodb+srv://osnaik23_db_user:fMXqxxDnHX3pvwX5@cluster0.pjxw7au.mongodb.net/day-6").then(() => {
+        console.log("Connected to MongoDB database");
+    }).catch((error) => {
+        console.error("Error connecting to MongoDB database:", error);
+    });
+}
+
+connectToDatabase();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
