60f006ffc03386592886b098a30e3a40c8218c3e
OM-Naik <osnaik23@gmail.com>
2026-04-16 23:05:21 +0530

    refactor: move database connection logic to separate file

--- Backend/Database/MongoDB-Day1/server.js
+++ Backend/Database/MongoDB-Day1/server.js
@@ -4,19 +4,20 @@ It is to start server and connect it to database.
 
 const app = require("./src/app") // app.js file ko import krna
 const mongoose = require("mongoose") // mongoose library ko import krna
+const connectToDatabase = require("./config/database") // database.js file ko import krna 
 
-const dns = require('node:dns/promises');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+// Below code is present in [./config/database.js] --> It is use for connect to MongoDB database
 
+// function connectToDatabase() {
+//     mongoose.connect("mongodb+srv://osnaik23_db_user:fMXqxxDnHX3pvwX5@cluster0.pjxw7au.mongodb.net/day-6").then(() => {
+//         console.log("Connected to MongoDB database");
+//     }).catch((error) => {
+//         console.error("Error connecting to MongoDB database:", error);
+//     });
+// }
 
-// connect to MongoDB database
-function connectToDatabase() {
-    mongoose.connect("mongodb+srv://osnaik23_db_user:fMXqxxDnHX3pvwX5@cluster0.pjxw7au.mongodb.net/day-6").then(() => {
-        console.log("Connected to MongoDB database");
-    }).catch((error) => {
-        console.error("Error connecting to MongoDB database:", error);
-    });
-}
+const dns = require('node:dns/promises'); // DNS servers ko set krna
+dns.setServers(['8.8.8.8', '1.1.1.1']); // Google's and Cloudflare's DNS servers
 
 connectToDatabase();
 
