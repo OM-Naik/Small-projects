a5daf87b1b25eea3de7f72cd176b2c1165cec24a
OM-Naik <osnaik23@gmail.com>
2026-04-18 21:55:20 +0530

    updated

--- Backend/Database/CRUD/src/app.js
+++ Backend/Database/CRUD/src/app.js
@@ -27,4 +27,18 @@ app.post("/notes", async (req, res) => {
     });
 })
 
+
+/**
+ * -GET /notes --> get all notes
+ */
+
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find();
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes,
+    });
+})
+
+
 module.exports = app
--- Backend/Database/CRUD/src/config/database.js
+++ Backend/Database/CRUD/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectToDatabase() {
-    mongoose.connect("process.env.MONGODB_URI").then(() => {
+    mongoose.connect(process.env.MONGODB_URI).then(() => {
         console.log("Connected to MongoDB database");
     }).catch((error) => {
         console.error("Error connecting to MongoDB database:", error);
